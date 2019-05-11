eb24e782293541b1d07f70740308a66c3270f5a2
gugutz <gugutz@gmail.com>
2019-05-11 19:38:01 -0300

    finished transfer controller and service

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -1,34 +1,24 @@
 class TransferController < ApplicationController
 
-  def transfer!
-    # TransferService.transfer!(current_user, transfer_params)
-    Account.transaction do
-      source_account = Account.find(params[:source_account_id]) 
-      destination_account = Account.find(params[:destination_account_id]) 
-      amount = params[:amount]
-      user = params[:user]
+  def transfer
 
-      source_account.withdraw!(params[:amount])
-      destination_account.deposit!(params[:amount])
+    TransferService.transfer!(transfer_params)
 
-      Audit.create!(user: user, source_account_id: source_account.id, destination_account_id: destination_account.id, amount: amount, transfered_at: DateTime.now)
-
-    render json: {
-      user: user,
-      source_account: source_account.id,
-      destination_account: destination_account.id,
-      amount: amount
-    }, status: :created, error: nil
-    end
+    response = {
+      data: transfer_params,
+      error: nil
+  }
+    render json: response, status: :created
 
   end
 
   private 
 
   def transfer_params
-    params.require(:transfer).permit(:user, :source_account_id, :destination_account_id, :amount)
+    params.permit(:user, :source_account_id, :destination_account_id, :amount)
   end
 
 end
+
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/balance', to: 'accounts#balance'
-  post '/transfer', to: 'transfer#transfer!'
+  post '/transfer', to: 'transfer#transfer'
 end
 
 
