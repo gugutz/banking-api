cb15ba5cbfaf9928d3dcd89ca8fe4883c4148a75
gugutz <gugutz@gmail.com>
2019-05-11 15:22:29 -0300

    add routes for HTTP requests to check for balance and make transfer

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :accounts
   resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/balance', to: 'accounts#balance'
+  post '/transfer', to: 'transfer#transfer!'
 end
+
+
