88114bbf3641034d757691e82a28258a382724e3
gugutz <gugutz@gmail.com>
2019-05-12 11:23:40 -0300

    fix indentation

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -39,13 +39,13 @@ class ClientsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_client
-      @client = Client.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_client
+    @client = Client.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def client_params
-      params.require(:client).permit(:name, :password)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def client_params
+    params.require(:client).permit(:name, :password)
+  end
 end
