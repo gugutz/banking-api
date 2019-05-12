caf8d6f1c38fa457502147bbd67f5654576fb77c
gugutz <gugutz@gmail.com>
2019-05-12 10:05:18 -0300

    allows transfer only for authenticated users

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -1,21 +1,26 @@
 class TransferController < ApplicationController
+  before_action :authenticate_client!
 
   def transfer
+    if client_signed_in?
+      TransferService.transfer!(transfer_params)
 
-    TransferService.transfer!(transfer_params)
-
-    response = {
-      data: transfer_params,
-      error: nil
-  }
-    render json: response, status: :created
+      response = {
+        data: transfer_params,
+        error: nil
+      }
+      render json: response, status: :created
+    else
+      logger.error("You must be authenticated to use this feature")
+      render json: {error: "you must authenticate to use the API"}, status: :unauthorized
+    end
 
   end
 
   private 
 
   def transfer_params
-    params.permit(:user, :source_account_id, :destination_account_id, :amount)
+    params.permit(:client, :source_account_id, :destination_account_id, :amount)
   end
 
 end
