0c9e0884dd58eaa2cc1fc579cd5bed9d06f3f4ac
gugutz <gugutz@gmail.com>
2019-05-12 21:43:42 -0300

    removed devise session routes and renamed custom controller session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   devise_for :clients
-  get 'sessions/login'
-  get 'sessions/logout'
   resources :accounts
   resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/balance', to: 'accounts#balance'
   post '/transfer', to: 'transfer#transfer'
-  post '/login', to: 'sessions#login'
-  delete '/logout', to: 'sessions#logout'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
 
 
