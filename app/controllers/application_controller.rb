2e0869146567c1dd654cd24f1c3fcd2701cb18cd
gugutz <gugutz@gmail.com>
2019-05-12 21:41:07 -0300

    removed devise configured parameters as they arent used anymore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::API
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:email, :password)
-    end
-  end
-
 end
+
